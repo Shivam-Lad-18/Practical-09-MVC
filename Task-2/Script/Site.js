f6a7d93af1ffffff2d15236de189c1ef2e4c6dd7
Shivam Lad <shivamvlad3@gmail.com>
2025-03-31 15:18:43 +0530

    Add project files.

---
+++ Task-2/Script/Site.js
@@ -0,0 +1,15 @@
+﻿ document.getElementById("printText").addEventListener("click", function () {
+    document.getElementById("text").innerText = "Hello World";
+});
+document.getElementById("bold").addEventListener("click", function () {
+    document.getElementById("text").style.fontWeight = "bold";
+});
+document.getElementById("italic").addEventListener("click", function () {
+    document.getElementById("text").style.fontStyle = "italic";
+});
+document.getElementById("underline").addEventListener("click", function () {
+    document.getElementById("text").style.textDecoration = "underline";
+});
+document.getElementById("reset").addEventListener("click", function () {
+    document.getElementById("text").style= "none";
+});
---
+++ Task-3.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Task_3;
+using Task_3.Controllers;
+
+namespace Task_3.Tests.Controllers
+{
+    // Using TestClass as an attribute to HomeControllerTest class
+    [TestClass]
+    public class HomeControllerTest
+    {
+        // Using TestMethod as an attribute to Index method
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);//Checking wether my view is not null
+            Assert.AreEqual("Hello, World!", result.ViewBag.Message); //Comparing two strings to check if they are equal
+        }
+    }
+}
